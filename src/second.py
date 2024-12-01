b1a7a1bd2813833254c96292698e91d887a01a7c
AlexTraveylan <monsieurdemares@gmail.com>
2024-12-01 06:42:18 +0100

    prepare second day
    import numpy as np
    
    from utils import main
    
    def code(s: str):
        # Your code here
    
        lines = s.split("\n")
        lines = np.array([[int(x) for x in line.split()] for line in lines])
        transposed_sorted = np.sort(lines.T, axis=1)
        sum_abs_dif = np.sum(np.abs(transposed_sorted[1] - transposed_sorted[0]))
    
        return sum_abs_dif
    
    def code(s: str):
        # Your code here
    
        lines = s.split("\n")
        lines = np.array([[int(x) for x in line.split()] for line in lines])
        transposed_sorted = np.sort(lines.T, axis=1).tolist()
        scores_dict = {k: v * k for k, v in Counter(transposed_sorted[1]).items()}
    
        similarity_counter = 0
        for number in transposed_sorted[0]:
            similarity_counter += scores_dict.get(number, 0)
    
        return similarity_counter
    
    if __name__ == "__main__":
        main(day=1, part=2, code=code)
    
    import numpy as np
    
    from utils import main
    
    def code(s: str):
        # Your code here
    
        lines = s.split("\n")
        lines = np.array([[int(x) for x in line.split()] for line in lines])
        transposed_sorted = np.sort(lines.T, axis=1)
        sum_abs_dif = np.sum(np.abs(transposed_sorted[1] - transposed_sorted[0]))
    
        return sum_abs_dif
    
    def code(s: str):
        # Your code here
    
        lines = s.split("\n")
        lines = np.array([[int(x) for x in line.split()] for line in lines])
        transposed_sorted = np.sort(lines.T, axis=1).tolist()
        scores_dict = {k: v * k for k, v in Counter(transposed_sorted[1]).items()}
    
        similarity_counter = 0
        for number in transposed_sorted[0]:
            similarity_counter += scores_dict.get(number, 0)
    
        return similarity_counter
    
    if __name__ == "__main__":
        main(day=1, part=2, code=code)
    
    import numpy as np
    
    from utils import main
    
    def code(s: str):
        # Your code here
    
        lines = s.split("\n")
        lines = np.array([[int(x) for x in line.split()] for line in lines])
        transposed_sorted = np.sort(lines.T, axis=1)
        sum_abs_dif = np.sum(np.abs(transposed_sorted[1] - transposed_sorted[0]))
    
        return sum_abs_dif
    
    def code(s: str):
        # Your code here
    
        lines = s.split("\n")
        lines = np.array([[int(x) for x in line.split()] for line in lines])
        transposed_sorted = np.sort(lines.T, axis=1).tolist()
        scores_dict = {k: v * k for k, v in Counter(transposed_sorted[1]).items()}
    
        similarity_counter = 0
        for number in transposed_sorted[0]:
            similarity_counter += scores_dict.get(number, 0)
    
        return similarity_counter
    
    if __name__ == "__main__":
        main(day=1, part=2, code=code)

---
+++ src/second.py
@@ -0,0 +1,24 @@
+from utils import main
+
+# ================================
+# PART 1
+# ================================
+
+
+def code(s: str):
+    # Your code here
+
+    return 0
+
+
+# ================================
+# PART 2
+# ================================
+
+
+# def code(s: str):
+#     # Your code here
+#     return 0
+
+if __name__ == "__main__":
+    main(day=2, part=1, code=code)
