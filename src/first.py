8dc7163353e04be21977af29daaf93b98f437e32
AlexTraveylan <monsieurdemares@gmail.com>
2024-12-01 06:41:13 +0100

    first day

--- src/first.py
+++ src/first.py
@@ -1,11 +1,44 @@
-from utils import ints, main
+from collections import Counter
+
+import numpy as np
+
+from utils import main
+
+# ================================
+# PART 1
+# ================================
 
 
 def code(s: str):
     # Your code here
-    
-    return 1234
+
+    lines = s.split("\n")
+    lines = np.array([[int(x) for x in line.split()] for line in lines])
+    transposed_sorted = np.sort(lines.T, axis=1)
+    sum_abs_dif = np.sum(np.abs(transposed_sorted[1] - transposed_sorted[0]))
+
+    return sum_abs_dif
+
+
+# ================================
+# PART 2
+# ================================
+
+
+def code(s: str):
+    # Your code here
+
+    lines = s.split("\n")
+    lines = np.array([[int(x) for x in line.split()] for line in lines])
+    transposed_sorted = np.sort(lines.T, axis=1).tolist()
+    scores_dict = {k: v * k for k, v in Counter(transposed_sorted[1]).items()}
+
+    similarity_counter = 0
+    for number in transposed_sorted[0]:
+        similarity_counter += scores_dict.get(number, 0)
+
+    return similarity_counter
 
 
 if __name__ == "__main__":
-    main(day=1, part=1, code=code)
+    main(day=1, part=2, code=code)
